3fd4cd1f45b4674d0b131494d0386acbafe30ca5
Thyiad <1520583107@qq.com>
2021-05-19 21:13:02 +0800

    feat: release 0.0.4

--- README.md
+++ README.md
@@ -2,30 +2,42 @@
 
 把目录打包成离线缓存要求的目录结构
 
-例如：
+### 安装
+
+```bash
+npm install -D @dm/offline-webpack-plugin
+```
+
+### 使用
+
+在你的 webpack 配置中追加一个 plugin：
 
 ```js
-const { DMOfflineWebpackPlugin } = require('../lib/index');
+const { DMOfflineWebpackPlugin } = require('@dm/offline-webpack-plugin');
 
 new DMOfflineWebpackPlugin({
     // 源目标，默认dist
     src: 'dist',
-    // 目标文件名，默认offline，会生成一个offline.zip
-    target: 'offline',
-    // 当前项目所在域名，不需要schema，可有子路径，例如：dm.zhongan.com/m/run-app
-    domain: 'dm.zhongan.com',
-    // 子文件的mapping，可以是文件夹/文件
+    // 目标文件名，默认 offline （会生成一个offline.zip）
+    zipFileName: 'offline',
+    // 当前项目所在域名，不需要schema，可有子路径，例如：project.zhongan.com/foo/bar
+    destUrl: 'project.zhongan.com',
+    // dist目录的子文件映射表
     mapping: [
         {
             src: 'assets/images',
-            target: 'images',
-            domain: 'static.zhongan.com/website/m/mible/activity/dm',
-        },
-        {
-            src: 'assets/html',
-            target: 'htmlxxx',
-            domain: 'static.zhongan.com/website/m/mible/activity/dm',
+            destUrl: 'cdn.zhongan.com/foo/bar/images',
         },
     ],
 });
 ```
+
+### 举个栗子
+
+以上述配置为例
+
+dist 目录：
+![](./preview-dist.png)
+
+生成的 offline 目录：
+![](./preview-offline.png)
--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,11 @@ interface Options {
     /**
      * ，默认为 offline
      */
-    target?: string;
+    zipFileName?: string;
     /**
-     * 域名，不需要 schema
+     * 目标路径，不需要 schema
      */
-    domain: string;
+    destUrl: string;
     /**
      * 路径映射
      */
@@ -30,13 +30,9 @@ interface Options {
          */
         src: string;
         /**
-         * 目标目录
+         * 目标路径，不需要 schema
          */
-        target: string;
-        /**
-         * 域名，不需要 schema
-         */
-        domain: string;
+        destUrl: string;
     }[];
     /**
      * 压缩配置
@@ -74,20 +70,20 @@ class DMOfflineWebpackPlugin {
 
     prepare() {
         this.options.src = this.options.src || 'dist';
-        this.options.target = this.options.target || 'offline';
+        this.options.zipFileName = this.options.zipFileName || 'offline';
         this.options.src = path.resolve(cwd, this.options.src);
-        this.options.target = path.resolve(cwd, this.options.target);
+        this.options.zipFileName = path.resolve(cwd, this.options.zipFileName);
         this.options.mapping = this.options.mapping || [];
 
-        this.options.domain = this.options.domain.replace('http://', '').replace('https://', '').replace('//', '');
+        this.options.destUrl = this.options.destUrl.replace('http://', '').replace('https://', '').replace('//', '');
         this.options.mapping.forEach((item) => {
-            item.domain = item.domain.replace('http://', '').replace('https://', '').replace('//', '');
+            item.destUrl = item.destUrl.replace('http://', '').replace('https://', '').replace('//', '');
             item.src = path.resolve(this.options.src, item.src);
-            item.target = path.resolve(this.options.target, item.domain, item.target);
+            item.destUrl = path.resolve(this.options.zipFileName, item.destUrl);
         });
 
-        fs.removeSync(this.options.target);
-        fs.removeSync(this.options.target + '.zip');
+        fs.removeSync(this.options.zipFileName);
+        fs.removeSync(this.options.zipFileName + '.zip');
     }
 
     copyFiles() {
@@ -97,26 +93,26 @@ class DMOfflineWebpackPlugin {
                 this.message('warn', `${item.src} 不存在`);
                 return;
             }
-            fs.copySync(item.src, item.target);
+            fs.copySync(item.src, item.destUrl);
         });
         // 其他文件复制到主域名中
         const files = fs.readdirSync(this.options.src);
         files.forEach((file) => {
             const filePath = path.resolve(this.options.src, file);
-            const destPath = path.resolve(this.options.target, this.options.domain, file);
+            const destPath = path.resolve(this.options.zipFileName, this.options.destUrl, file);
             fs.copySync(filePath, destPath);
             // 把mapping中的文件删掉
             this.options.mapping.forEach((item) => {
                 const mappingName = path.relative(this.options.src, item.src);
-                const delPath = path.resolve(this.options.target, this.options.domain, mappingName);
+                const delPath = path.resolve(this.options.zipFileName, this.options.destUrl, mappingName);
                 fs.removeSync(delPath);
             });
         });
     }
 
     zipFile() {
-        const srcDir = this.options.target;
-        const targetZip = this.options.target + '.zip';
+        const srcDir = this.options.zipFileName;
+        const targetZip = this.options.zipFileName + '.zip';
 
         if (!fs.existsSync(srcDir)) {
             this.message('warn', `${srcDir} 不存在`);
@@ -128,7 +124,7 @@ class DMOfflineWebpackPlugin {
 
         output.on('close', () => {
             this.message('success', 'zip文件总大小 ' + getSimpleFileSize(archive.pointer()) + '\n');
-            fs.removeSync(this.options.target);
+            fs.removeSync(this.options.zipFileName);
         });
 
         archive.on('error', (err) => {
